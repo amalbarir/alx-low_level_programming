65856ff06e6f6c65f10b33a96bac08040a4ca800
amalbarir <a.barir1991@gmail.com>
2023-03-19 05:56:08 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main - entry point
+  *Description: print lowercase alphabet
+  *Return: always 0
+  */
+int main(void)
+{
+	char i, z;
+
+	i = 'a';
+	z = 'z';
+
+	while (i <= z)
+	{
+		putchar(i);
+		i++;
+	}
+	i = 'A';
+	z = 'Z';
+
+	while (i <= z)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
